6d689ac8dbce9e9f5cbd873dc617b202781ce9a1
molivaresz <molivaresz1@gmail.com>
2024-05-08 22:11:35 -0400

    add: uso de context en gallery y modifica validacion en filled de iconheart

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,42 @@
+import { useContext } from "react";
+import {PhotosContext} from "../context/PhotosContextProvider";
+import IconHeart from "./IconHeart";
+
 const Gallery = () => {
-  return <div className="gallery grid-columns-5 p-3"></div>;
+
+  const {data, setData} = useContext(PhotosContext)
+
+  const agregarFavorito =(id)=>{
+
+    const dataActualizada = data.map(dataPhotos =>{
+        if(dataPhotos.id === id){
+            return{
+                ...dataPhotos, liked: !dataPhotos.liked
+            }
+        }
+        return dataPhotos;
+    })
+
+    setData(dataActualizada)
+  }
+
+  return ( 
+  <>
+  <div className="gallery grid-columns-5 p-3">
+    {
+      data && 
+      data.map((dataPhotos, index) => (
+        <div key={'div1' + index} className="cuadroimagen">
+          <button type="button" onClick={()=> agregarFavorito(dataPhotos.id)}>
+            <img key={'img' + index} src={dataPhotos.src.tiny} alt={dataPhotos.alt} />
+            {dataPhotos.liked === false ? <IconHeart filled="0"/> : <IconHeart filled="1"/>}
+            <div key={'p' + index} className="textoimg">{dataPhotos.alt}</div>
+          </button>
+        </div>
+      ))
+    }
+  </div>
+  </>
+  )
 };
 export default Gallery;
