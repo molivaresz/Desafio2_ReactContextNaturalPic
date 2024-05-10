81f2b432629d7ae7a40bdd25f177a11a47e8e5cd
rolandobrice <rolandojose1109@gamil.com>
2024-05-09 21:04:15 -0400

    add: logica en favorites y se agrega clases en index.css

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,9 +1,41 @@
+import { useContext } from "react";
+import {PhotosContext} from "../context/PhotosContextProvider";
+import IconHeart from "../components/IconHeart";
 const Favorites = () => {
-  return (
-    <div>
+  const {data, setData} = useContext(PhotosContext)
+  const agregarFavorito =(id)=>{
+    const dataActualizada = data.map(dataPhotos =>{
+        if(dataPhotos.id === id){
+            return{
+                ...dataPhotos, liked: !dataPhotos.liked
+            }
+        }
+        return dataPhotos;
+    })
+    setData(dataActualizada)
+  }
+
+  return ( 
+    <>
+    <div className="App">
       <h1>Fotos favoritas</h1>
-      <div className="p-3 gallery grid-columns-4"></div>
+    </div>    
+    <div className="gallery grid-columns-5 p-3">
+      {
+        data && 
+        data.filter(dataPhotos => dataPhotos.liked == true).map((dataPhotos, index) => (
+  
+          <div key={'div1' + index} className="cuadroimagen">
+            <button type="button" onClick={()=> agregarFavorito(dataPhotos.id)}>
+              <img key={'img' + index} src={dataPhotos.src.tiny} alt={dataPhotos.alt} />
+              {dataPhotos.liked === false ? <IconHeart filled="0"/> : <IconHeart filled="1"/>}
+              <div key={'p' + index} className="textoimg">{dataPhotos.alt}</div>
+            </button>
+          </div>
+        ))
+      }
     </div>
-  );
+    </>
+  )
 };
 export default Favorites;
